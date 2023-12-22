db43792363d784c183662965bad2dfffd6d759ca
Lucas Laurentino <laurentino14@outlook.com>
2023-12-22 16:59:45 -0300

    hotfix: bitbucket pipeline

--- scripts/tags/build.sh
+++ scripts/tags/build.sh
@@ -1,4 +1,4 @@
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-docker build -f build/Dockerfile -t "$DOCKER_IMAGE:latest".
+docker build -f build/Dockerfile -t "$DOCKER_IMAGE:$BITBUCKET_TAG" .
 docker push "$DOCKER_IMAGE:latest"
 
